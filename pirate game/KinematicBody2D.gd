cc20581704889acd86f2fb710b058360b3b07f57
ThomasLalo <37088796+ThomasLalo@users.noreply.github.com>
2019-05-12 21:14:35 -0500

    movement stuff, tilesets stuff, and enemy animations

--- pirate game/KinematicBody2D.gd
+++ pirate game/KinematicBody2D.gd
@@ -5,26 +5,67 @@ const GRAVITY = 20
 const SPEED = 200
 const JUMP_HEIGHT = -600
 var motion = Vector2()
+var facingFlag = false #false is right, true is left
+
+func moveRight():
+	motion.x = SPEED
+	facingFlag = false
+	$Sprite.flip_h = false
+	
+
+func moveLeft():
+	motion.x = -SPEED
+	facingFlag = true
+	$Sprite.flip_h = true
 
 func _physics_process(delta):
 	motion.y += GRAVITY
-	
-	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
-		$Sprite.flip_h = false
-		$Sprite.play("run")
-	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
-		$Sprite.flip_h = true
-		$Sprite.play("run")
-	else:
-		motion.x = 0
-		$Sprite.play("idle")
-		
+
+	#ground animations
 	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
+		#run
+		if Input.is_action_pressed("ui_right"):
+			moveRight()
+			$Sprite.play("run")
+		elif Input.is_action_pressed("ui_left"):
+			moveLeft()
+			$Sprite.play("run")
+			
+		#jump
+		elif Input.is_action_pressed("ui_up"):
+			$Sprite.flip_h = facingFlag
+			$Sprite.play("jumpAnticip")
+			#motion.x = 0 
+			
+		elif Input.is_action_just_released("ui_up"):
 			motion.y = JUMP_HEIGHT
 
-		
+		#idle
+		else:
+			motion.x = 0
+			$Sprite.play("idle")
+
+	#air animations
+	if not is_on_floor():
+		#jumping animation
+		if  motion.y < 0:
+			$Sprite.flip_h = facingFlag
+			$Sprite.play("jump")
+			if Input.is_action_pressed("ui_right"):
+				moveRight()
+			elif Input.is_action_pressed("ui_left"):
+				moveLeft()
+			
+		#falling animation
+		elif  motion.y >= 0:
+			$Sprite.flip_h = facingFlag
+			$Sprite.play("fall")
+			if Input.is_action_pressed("ui_right"):
+				moveRight()
+			elif Input.is_action_pressed("ui_left"):
+				moveLeft()
+	
 	motion = move_and_slide(motion, UP)
 	pass
+	
+
