f257c7c056eced3edbe174397bf38a1083835578
ThomasLalo <37088796+ThomasLalo@users.noreply.github.com>
2019-05-26 14:22:46 -0500

    added a simple patrol to the pirate npc

--- pirate game/enemyPirate.gd
+++ pirate game/enemyPirate.gd
@@ -9,7 +9,7 @@ var motion = Vector2()
 
 # counter to allow for implemetation of jumpAnticip
 var jumpFrameCount = 0
-const JUMP_PADDING = 4 # I changed this to a const for to make the code more readable for my own sake
+const JUMP_PADDING = 4  # I changed this to a const for to make the code more readable for my own sake
 var inAirFrames = 0
 
 # counter to smooth out jumping
@@ -18,6 +18,35 @@ const END_JUMP_PADDING = 6
 
 var inAirLeftCount = 0
 var inAirRightCount = 0
+
+
+# enemy Patrolling counters and flags
+var patrolMoveCount = 0
+const FULL_MOVE_FRAMES = 208 # run the run animation loop 16 times
+const HALF_MOVE_FRAMES = 104
+
+var patrolIdleCount = 0
+const FULL_IDLE_FRAMES = 132 # run the idle animation loop 2 times
+const HALF_IDLE_FRAMES = 66
+
+var partolFlag = true
+var patrolSequence = 0
+
+###----------------------------------------------------------------###
+# PATROLLING SEQUENCE
+#
+# 0, 1, and 2 are the initial movements
+# 3, 4, 5, and 6 are in a loop, after step 3 will play after step 6
+#
+# 0 = 33 idle frames, half of the regular idle
+# 1 = 104 right movement frames, half of regular movement
+# 2 = 33 idle frames
+#
+# 3 = 208 left movement frames, regular movement amount
+# 4 = 66 idle frames, regular idle amount
+# 5 = 208 right movement frames
+# 6 = 66 idle frames
+###----------------------------------------------------------------###
 
 
 func moveRight():
@@ -38,20 +67,70 @@ func _physics_process(delta):
 		motion.y += GRAVITY
 	
 	# running movement, animation, and facing direction
-	if Input.is_action_pressed("ui_right"):
-		moveRight()
-		if is_on_floor():
-			$Sprite.play("run")
-	if Input.is_action_pressed("ui_left"):
-		moveLeft()
-		if is_on_floor():
-			$Sprite.play("run")
-	
-	# idle animation when not moving and on ground
-	if motion.x == 0 and is_on_floor() and jumpFrameCount == 0:
-		$Sprite.play("idle")
+	if partolFlag:
+		match patrolSequence:
+			0:
+				if patrolIdleCount <= HALF_IDLE_FRAMES:
+					$Sprite.play("idle")
+					patrolIdleCount += 1
+				else:
+					patrolIdleCount = 0
+					patrolSequence = 1
+			1:
+				if patrolMoveCount <= HALF_MOVE_FRAMES:
+					moveRight()
+					if is_on_floor():
+						$Sprite.play("run")
+					patrolMoveCount += 1
+				else:
+					patrolMoveCount = 0
+					patrolSequence = 2
+
+			2:
+				if patrolIdleCount <= HALF_IDLE_FRAMES:
+					$Sprite.play("idle")
+					patrolIdleCount += 1
+				else:
+					patrolIdleCount = 0
+					patrolSequence = 3
+
+			3:
+				if patrolMoveCount <= FULL_MOVE_FRAMES:
+					moveLeft()
+					if is_on_floor():
+						$Sprite.play("run")
+					patrolMoveCount += 1
+				else:
+					patrolMoveCount = 0
+					patrolSequence = 4
+			4:
+				if patrolIdleCount <= FULL_IDLE_FRAMES:
+					$Sprite.play("idle")
+					patrolIdleCount += 1
+				else:
+					patrolIdleCount = 0
+					patrolSequence = 5
+			5:
+				if patrolMoveCount <= FULL_MOVE_FRAMES:
+					moveRight()
+					if is_on_floor():
+						$Sprite.play("run")
+					patrolMoveCount += 1
+				else:
+					patrolMoveCount = 0
+					patrolSequence = 6
+			6:
+				if patrolIdleCount <= FULL_IDLE_FRAMES:
+					$Sprite.play("idle")
+					patrolIdleCount += 1
+				else:
+					patrolIdleCount = 0
+					patrolSequence = 3
+
+
+
 		
-	#jump initiated
+	""" #jump initiated
 	if Input.is_action_just_pressed("ui_up") and is_on_floor() and jumpFrameCount == 0:
 		$Sprite.play("jumpAnticip")
 		jumpFrameCount += 1
@@ -114,7 +193,7 @@ func _physics_process(delta):
 	
 	# if you are in the air and going down
 	if motion.y > 0 and not is_on_floor():
-		$Sprite.play("fall")
+		$Sprite.play("fall") """
 	
 	# update character position based on input from above,
 	# will move until a collision is detected
